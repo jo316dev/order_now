a71c63b435460336f78b1a8ea0f125ff476e5220
Ricardo <jo316.dev@gmail.com>
2022-02-11 10:07:24 -0300

    colluns plan profile permissions

---
+++ database/migrations/2022_02_11_100447_create_permission_profile_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_profile', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('permission_id')->constrained('permissions');
+            $table->foreignId('profile_id')->constrained('profiles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_profile');
+    }
+}
---
+++ database/migrations/2022_02_11_100503_create_plan_profile_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlanProfileTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plan_profile', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('plan_id')->constrained('plans');
+            $table->foreignId('profile_id')->constrained('profiles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plan_profile');
+    }
+}
